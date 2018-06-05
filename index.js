cb1bab16221126bef65cb4c81a5a915bff0c12a1
Shivansh <ss4328@drexel.edu>
2018-06-05 04:13:52 +0000

    Changes to views and server

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 var express = require("express");
 var app = express();
 
-// var mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost/test");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test");
 
-// var workoutSchema = new mongoose.Schema({
-//     workoutName: String,
-//     videoLink: String,
-//     Rating: Number,
-//     QuickFacts: {
-//                 MusclesWorked: String,
-//                 Type: String,
-//                 SkillLevel: String,
-//                 GeneralRemark: String
-//     },
-//     WorkoutLoad: {
-//                 EasyCount: Number,
-//                 MediumCount: Number,
-//                 HardCount: Number
-//     },
-//     imagelink: String,
-//     workoutInstructions: [{type: String}],
-//     tips: [{type: String}]
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var workoutSchema = new mongoose.Schema({
+    workoutName: String,
+    videoLink: String,
+    Rating: Number,
+    QuickFacts: {
+                MusclesWorked: String,
+                Type: String,
+                SkillLevel: String,
+                GeneralRemark: String
+    },
+    WorkoutLoad: {
+                EasyCount: Number,
+                MediumCount: Number,
+                HardCount: Number
+    },
+    imagelink: String,
+    workoutInstructions: [{type: String}],
+    tips: [{type: String}]
     
-// })
+})
 
-// var workoutModel = mongoose.model("WORKOUTSCHEMA",workoutSchema); 
+var workoutModel = mongoose.model("WORKOUTSCHEMA",workoutSchema); 
 
-// var workoutExample=new workoutModel({workoutName: "Bench-PressFROMDB", videoLink:"", Rating: 1, QuickFacts:{MusclesWorked:"a", type:"B", SkillLevel:"C", GeneralRemark:"D"}, WorkoutLoad: {EasyCount:10, MediumCount:20, HardCount:30}, imagelink:"", workoutInstructions:["a"], tips:["b"]});
+var workoutExample=new workoutModel({workoutName: "Bench-PressFROMDB", videoLink:"", Rating: 1, QuickFacts:{MusclesWorked:"a", type:"B", SkillLevel:"C", GeneralRemark:"D"}, WorkoutLoad: {EasyCount:10, MediumCount:20, HardCount:30}, imagelink:"", workoutInstructions:["a"], tips:["b"]});
 
 
 //dont uncomment this
@@ -45,7 +49,6 @@ var app = express();
 
 
 
-
 // var testSchema = new mongoose.Schema({
 //     content1: String,
 //     content2: Number,
@@ -59,11 +62,14 @@ var app = express();
 //     if(err){
 //         console.log("ERROR GENERATED!");
 //     }
-//     else{
+//     else{    
 //         console.log("WE SAVED THE OBJECT THE THE DB");
 //         console.log(testExample);
 //     }
 // });
+
+
+
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -84,18 +90,69 @@ app.get("/dbadd", function(req,res){
     res.render('dbadd');
 });
 
+app.post("/populateTables", function(req,res){
+    console.log("reached populateTables");
+    
+    var workoutName = req.body.workoutName;
+    var tutorialLink  = req.body.tutorialLink;
+    var displayImage = req.body.displayImage;
+    var rating  = req.body.rating;
+    
+    var musclesWorked = req.body.musclesWorked;
+    var workoutType =req.body.workoutType;
+    var skillLevel=req.body.skillLevel;
+    var generalRemarks=req.body.generalRemarks;
+    
+    var easyCount=req.body.easyCount;
+    var mediumCount=req.body.mediumCount;
+    var hardCount =req.body.hardCount;
+    
+    var relworkout1 = req.body.relworkout1;
+    var relworkout2 = req.body.relworkout2;
+    var relworkout3 = req.body.relworkout3;
+    
+    var ins1 = req.body.ins1;
+    var ins2 = req.body.ins2;
+    var ins3 = req.body.ins3;
+    var ins4 = req.body.ins4;
+    
+    var tip1 =req.body.tip1;
+    var tip2 =req.body.tip2;
+    var tip3 =req.body.tip3;
+    var tip4 =req.body.tip4;
+    
+    var workoutToAdd=new workoutModel({workoutName: workoutName, videoLink:tutorialLink, Rating: rating, QuickFacts:{MusclesWorked:musclesWorked, type:workoutType, SkillLevel:skillLevel, GeneralRemark:generalRemarks}, WorkoutLoad: {EasyCount:easyCount, MediumCount:mediumCount, HardCount:hardCount}, imagelink:displayImage, workoutInstructions:[ins1, ins2, ins3, ins4], tips:[tip1,tip2,tip3,tip4]});
+    
+    
+    workoutToAdd.save(function(err, cat){
+    if(err){
+        console.log("ERROR GENERATED!: "+err);
+    }
+    else{    
+        console.log("WE SAVED THE OBJECT THE THE DB");
+        console.log(workoutToAdd);
+    }
+});
+    
+    console.log(req.body.mainForm);
+    console.log('Workout Name: '+ workoutName);
+    console.log((req.xhr).toString());
+    res.redirect('/dbadd');
+})
+
 app.get("/workout", function(req,res){
-    // workoutModel.find({}, function(err, obj){
-    //     if(err){
-    //         console.log("OH NO! We got an error here!");
-    //     }
-    //     else{
-    //         console.log(obj);
-    //         console.log(obj[0].workoutName);
-    //         res.render('workout', {objpassed:obj});
-    //     }
-    // })
-   res.render('workout'); 
+    workoutModel.findById("5adba18f1c6b080d94f7b4bf", function(err, obj){
+        if(err){
+            console.log("OH NO! We got an error here!");
+        }
+        else{
+            console.log("No error connecting to DB");
+            console.log(obj);
+            console.log(obj.workoutName);
+            res.render('workoutTemplate', {objpassed:obj});
+        }
+    })
+   //res.render('workoutTemplate'); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
--- public/js/submitToDb.js
+++ public/js/submitToDb.js
@@ -1,24 +1,50 @@
 /* global $ */
 
-function submitDataToDB(){
-	alert("HEY!");
-	console.log("hey");
+// function submitDataToDB(){
 	
- //   var dropdown = $('#options').get(0);
-	// var opt = dropdown.options[dropdown.selectedIndex].value;
+// 	// var data = new FormData();
+// 	// data.append('user', 'person');
+// 	// data.append('pwd', 'password');
+// 	// data.append('organization', 'place');
+// 	// data.append('requiredkey', 'key');
+	
+// 	var http = new XMLHttpRequest();
+// 	var url = "/populateTables";
+// 	var params = "lorem=ipsum&name=binny";
+// 	http.open("POST", url, true);
+	
+// 	//Send the proper header information along with the request
+// 	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	
+// 	http.onreadystatechange = function() {//Call a function when the state changes.
+// 	    if(http.readyState == 4 && http.status == 200) {
+// 	        alert(http.responseText);
+// 	    }
+// 	    else{
+// 	    	alert('error');
+// 	    }
+// 	}
+// 	http.send(params);
+// }	
+	
+	// alert("HEY!");
 	// var URL = '/populateTables';
+	// var name = $('#workoutName').val();
 	// $.ajax({
 	// 	type: 'POST',
-	// 	url: URL,
-	// 	data: {
-	// 		't': opt
-	// 	},
-	// 	dataType: 'html',
-	// 	success: function(msg) {
-	// 		$('#outputTables').html(msg);
-	// 	},
-	// 	error: function(xhr, ajaxOptions, thrownError) {
-	// 		alert('Error while connecting to the server!');
-	// 	}
+ //       url: URL,
+ //       dataType: 'html',
+ //       data: 
+ //       {
+ //           'name': name,
+ //       },
+ //       success: function(msg) {
+ //           alert('success');
+ //       },
+ //       error: function(xhr, ajaxOptions, thrownError) {
+ //           console.log(thrownError);
+ //           console.log(xhr.responseText);
+ //           alert('Error while connecting to the server!' + name);
+ //       }
 	// });
-}
+//}
